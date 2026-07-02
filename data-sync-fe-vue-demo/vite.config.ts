7473e95eaf70588ba6ec9ebc299439b536864fe7
ly <1563694059@qq.com>
2026-07-02 10:42:43 +0800

    暂存初版ui

--- data-sync-fe-vue-demo/vite.config.ts
+++ data-sync-fe-vue-demo/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
